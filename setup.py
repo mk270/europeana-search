d649077a364fb9a6b457951136b82e9999005124
Martin Keegan <martin@no.ucant.org>
2013-03-17 01:17:46 +0000

    get dependency version right this time

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='europeana-search',
-    version='0.1.2',
+    version='0.1.3',
     author='Martin Keegan',
     author_email='martin@no.ucant.org',
     packages=['europeana'],
@@ -12,6 +12,6 @@ setup(
     description='Basic Python wrapper for Europeana Search API.',
     long_description=open('README.txt').read(),
     install_requires=[
-        "requests >= 0.5.0"
+        "requests >= 1.1.0"
     ],
 )
